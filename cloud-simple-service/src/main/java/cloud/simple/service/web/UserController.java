c3d36685418a29a5cac93b299776228218477688
heqf <happiness_he@126.com>
2017-11-18 14:20:43 +0800

    提交

--- cloud-simple-service/src/main/java/cloud/simple/service/web/UserController.java
+++ cloud-simple-service/src/main/java/cloud/simple/service/web/UserController.java
@@ -1,8 +1,10 @@
 package cloud.simple.service.web;
 
+import cloud.simple.service.conf.DataSourceProperties;
 import cloud.simple.service.domain.UserService;
 import cloud.simple.service.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +14,23 @@ import java.util.List;
 @RestController
 public class UserController {
 
+	//@Value("${spring.redis.host}")
+	private String redisHost;
+
+	//@Value("${my.name}")
+	private String name;
+
 	@Autowired
 	UserService userService;
+
+	@Autowired
+	DataSourceProperties dataSourceProperties;
 	
 	@RequestMapping(value="/user",method=RequestMethod.GET)
 	public List<User> readUserInfo(){
-		List<User> ls=userService.searchAll();		
+		System.out.println("host is "+ redisHost);
+		System.out.println("my name is  "+ name);
+		List<User> ls=userService.searchAll();
 		return ls;
 	}
 }
